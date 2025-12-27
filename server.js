ad3ae6634c1b42536f43f1feb67dd42329810f6a
Chidozie Kenneth <abuguchidoziekenneth@gmail.com>
2025-12-27 02:44:00 -0800

    ping-pong route added

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.get("/", (req, res) =>{
-    res.send("Server is running ")
+app.get("/ping", (req, res) =>{
+    res.json({message :"pong"})
 });
 
 
